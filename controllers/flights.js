fa09a2ee9764c72a00dea43a42ae0bb237b778e3
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-16 10:08:05 -0400

    post and delete functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,40 @@
 import { Flight } from "../models/flight.js"
 
 function index(req, res) {
-  res.render('flights/index', { title: 'All Flights' })
+  Flight.find({})
+  .then( flights => {
+    res.render('flights/index', { 
+      title: 'All Flights',
+      flights: flights
+    })
+  })
+}
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: "Add Flight",
+  })
 }
 
+function create(req, res){
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+}
+
+function flightDelete(req, res) {
+  Flight.findByIdAndDelete(req.params.flightId)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
 export {
   index,
+  newFlight as new,
+  create,
+  flightDelete as delete,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,9 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 // GET localhost:3000/users
+router.get('/new', flightsCtrl.new)
 router.get('/', flightsCtrl.index)
+router.post('/', flightsCtrl.create)
+router.delete('/:flightId', flightsCtrl.delete)
 
 export { router }
